c29c74b9b43d254c5cce202fcffde0739b97e03d
xdeepakv <deepak.m.shrma@gmail.com>
2020-07-13 02:28:06 +0800

    Deploy website - based on a5d8fedd4b9ba0f0efcf58e97a4a3e1a53ed783d

--- 05b30e76.75918663.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{100:function(n){n.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"inner-feeling","name":"inner-feeling","count":2,"permalink":"/blog/tags/inner-feeling"}')}}]);
---
+++ 1df93b7f.964e9bda.js
@@ -0,0 +1,36 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{132:function(e,t,r){"use strict";r.r(t);r(152);var n=r(197),a=r(0),l=r.n(a),o=r(147),c=r(144),i=r(139),m=r(138),s=r(142);const u=e=>t=>t.theme[e],d=o.b`
+  p, h1, h2, h3, h4, h5, h6 {
+    margin: 0;
+    padding: 0;
+  }
+`;var p={color_primary:"#e54e79",color_primary_dark:"#e54e79",color_primary_darker:"#df265b",color_primary_darkest:"#bb1c49",color_primary_light:"#e9698d",color_primary_lighter:"#eb7697",color_primary_lightest:"#f19eb6",margin_md:"8px",margin_sm:"4px",margin_lg:"12px",bg_color:"#fff",bg_color_invert:"#000"};var g=o.c.button`
+  border: 1px solid ${u("color_primary_darker")};
+  background: ${u("bg_color")};
+  height: 40px;
+  min-width: 100px;
+  color: ${u("color_primary")};
+  cursor: pointer;
+  font-weight: 600;
+  border-radius: ${u("margin_sm")};
+  outline: none;
+  box-shadow: none;
+`;o.c.p`
+  color: ${e=>e.theme.color_primary};
+`;const b=o.c.h2`
+  color: ${e=>e.theme.color_primary};
+`,h=o.c.h4`
+  color: ${e=>e.theme.color_primary};
+`,_=o.c.ul`
+  color: ${e=>e.theme.color_primary_darker};
+  list-style: square;
+`;const E=o.c.div`
+  padding: 10px 20px;
+  background: ${u("bg_color")};
+  min-height: 100vh;
+`,f=o.c.div`
+  height: 1px;
+  background: ${u("color_primary")};
+  margin: ${u("margin_sm")} 0 ${u("margin_md")} 0;
+`,k=o.c.div`
+  height: ${u("margin_lg")};
+`;function y(){var e=Object(n.a)(["\n  position: fixed;\n  bottom: 0;\n  height: 60px;\n  border-top: 1px solid ",";\n  width: 100%;\n  display: flex;\n  align-items: center;\n  justify-content: space-around;\n"]);return y=function(){return e},e}function v(){var e=Object(n.a)(["\n  display: flex;\n  justify-content: flex-end;\n  align-items: center;\n"]);return v=function(){return e},e}var w=o.c.div(v()),x=o.c.div(y(),u("color_primary_darkest")),j=[Object.assign(Object.assign({},p),{},{bg_color:p.bg_color_invert}),p];t.default=function(){var e=Object(m.a)().siteConfig,t=void 0===e?{}:e,r=Object(a.useState)(j[1]),n=r[0],u=r[1];return l.a.createElement(o.a,{theme:function(){return n}},l.a.createElement(d,null),l.a.createElement(c.a,{title:"Hello from "+t.title,description:""+t.tagline},l.a.createElement(E,null,l.a.createElement(w,null,l.a.createElement(g,{style:{marginRight:12},onClick:function(){u(j[0])}},"Tech"),l.a.createElement(g,{onClick:function(){u(j[1])}},"Non-Tech")),l.a.createElement(b,null,"About Me"),l.a.createElement(f,null),l.a.createElement(h,null,t.tagline),l.a.createElement(_,null,l.a.createElement("li",null,"Polyglot Programmer Efficiently can work on UI/UX(Frontend), Backend and Mobile apps"),l.a.createElement("li",null,"Mobile: Android, Flutter, React Native, Swift(Beginner)"),l.a.createElement("li",null,"Frontend: React, Angular, Typescript, Object-Oriented JavaScript, ES2015 and above"),l.a.createElement("li",null,"Backend: NodeJs, Kotlin, Go, SpringBoot, Rust"),l.a.createElement("li",null,"DevOps: Proficient on scripting languages(Groovy, bash, nodejs), Jenkin"),l.a.createElement("li",null,"Test-Driven Development using Jest, Karma-Jasmine, Protractor, Expresso, Mockito")),l.a.createElement(i.a,{to:Object(s.a)("docs/resume")},l.a.createElement(g,null," More"," >>>"," ")),l.a.createElement(k,null),l.a.createElement(b,null,"Blogs"),l.a.createElement(f,null),l.a.createElement(_,null,l.a.createElement(i.a,{to:Object(s.a)("/blog/one-for-all")},l.a.createElement("li",null,"How to ditch WebPack, Jest to write TypeScript/NPM library just using Deno")),l.a.createElement(i.a,{to:Object(s.a)("blog/welcome")},l.a.createElement("li",null,"Welcome"))),l.a.createElement(i.a,{to:Object(s.a)("blog")},l.a.createElement(g,null," More"," >>>"," ")),l.a.createElement(x,null,l.a.createElement("a",{href:"https://www.linkedin.com/in/xdeepakv/",target:"_blank"},"Linkedin"),l.a.createElement("a",{href:"https://medium.com/@deepak_v",target:"_blank"},"Medium"),l.a.createElement("a",{href:"https://discord.com/channels/@deepakv",target:"_blank"},"Discord"),l.a.createElement("a",{href:"https://github.com/deepakshrma/",target:"_blank"},"GitHub")))))}}}]);

dcab3a42caf090a6390d6bd6946ce47ea3a1b24f
xdeepakv <deepak.m.shrma@gmail.com>
2020-08-27 04:13:15 +0800

    Deploy website - based on b18ccc75b5eb37d445585c5bacb0fbfe83373a11

---
+++ 07313258.490be7f2.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[6],{64:function(a){a.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"privacy","name":"privacy","count":1,"permalink":"/blog/tags/privacy"}')}}]);

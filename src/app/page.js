f4f66c2d429c12ebbd93585d9f430d233cf1c176
Deepak Vishwakarma[MS] <deepak.m.shrma@gmail.com>
2024-02-05 00:02:35 +0800

    doc: push to deploy

--- src/app/page.js
+++ src/app/page.js
@@ -67,7 +67,7 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-12">
       <h1 className="mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-        Deepak Vishwakarma
+        Deepak Vishwakarma, Singapore
       </h1>
       <h2 className="mb-4 text-2xl text-gray-900 md:text-3xl lg:text-4xl dark:text-white my-20">Showcase</h2>
       <div className="page-cards">
